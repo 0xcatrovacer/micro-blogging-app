70a8bda9462f025e7667ebb7e56ce176a1bafded
Swarnab Garang <swarnabgarang@gmail.com>
2021-02-20 19:15:29 +0530

    styled app, set up json server

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My Website', body: 'Lorem', author: 'swarnab', id: 1 },
-        { title: 'Welcome Pawri', body: 'Lorem', author: 'erudite', id: 2 },
-        { title: 'Web Dev Tips', body: 'Lorem', author: 'entrebay', id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState(null);
+
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json()
+            })
+            .then((data) => {
+                setBlogs(data);
+            })
+    }, []);
 
     return (
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blogPreview" key = {blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p> <strong>Written By:</strong> {blog.author}</p>
-                </div>
-            ))}
+            {blogs && <BlogList blogs={blogs} title='All Blogs' />}
         </div>
     );
 }
