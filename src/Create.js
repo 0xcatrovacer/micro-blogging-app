519eca7795bac945c0874d7c6123b3107be0a58c
Swarnab Garang <swarnabgarang@gmail.com>
2021-02-21 14:26:13 +0530

    minor changes

--- src/Create.js
+++ src/Create.js
@@ -5,7 +5,7 @@ const Create = () => {
 
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
-    const [author, setAuthor] = useState('Swarnab');
+    const [author, setAuthor] = useState('');
     const [isPending, setIsPending] = useState(false);
     const history = useHistory();
 
@@ -43,13 +43,12 @@ const Create = () => {
                     onChange={(e) => setBody(e.target.value)}
                 />
                 <label>Blog Author:</label>
-                <select
+                <input
+                    type="text"
+                    required
                     value={author}
-                    onChange={(e) => setAuthor(e.target.value)}>
-                    <option value="swarnab">Swarnab</option>
-                    <option value="erudite">Erudite</option>
-                    <option value="ira">Ira</option>
-                </select>
+                    onChange={(e) => setAuthor(e.target.value)}
+                />
                 {!isPending && <button>Add Blog</button>}
                 {isPending && <button disabled>Adding Blog...</button>}
             </form>
